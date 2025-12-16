1152bfe435e10ef7b2ce0f8f1ef60805489c5a7d
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2025-12-17 02:51:07 +0530

    feat: added new project card and refined hero spacing

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -7,7 +7,7 @@ export const personalInfo = {
     phone: "+91 9721403673",
     location: "Noida, Uttar Pradesh",
     cvLink: "./images/AshrafResume 1512.pdf", 
-    profileImg: "./images/AshrafImg.jpg", // Path relative to the 'public' folder
+    profileImg: <img src="./images/AshrafImg.jpg" alt="Profile Picture of Mohd Ashraf" />
 };
 
 export const socialLinks = [
